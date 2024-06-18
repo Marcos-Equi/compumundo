1e19a8a9e92279ca82599626c7bedf4463a49bd8
root <root@DESKTOP-HVH29MM.localdomain>
2024-06-18 16:43:07 -0300

    arreglo para config y models

---
+++ backend/config.py
@@ -0,0 +1,3 @@
+class Config:
+    SQLALCHEMY_DATABASE_URI = 'postgresql://benjamin:benja2024@localhost/componentes'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
---
+++ backend/models.py
@@ -0,0 +1,16 @@
+
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class Producto(db.Model):
+    __tablename__ = 'productos'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    nombre = db.Column(db.String(255), nullable=False)
+    tipo = db.Column(db.String(50), nullable=False)
+    precio = db.Column(db.Numeric(10, 2), nullable=False)
+    stock = db.Column(db.Integer, nullable=False)
+    descripcion = db.Column(db.Text)
+    
+
