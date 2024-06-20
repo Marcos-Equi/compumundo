74305fdf4571af9227733fc842c4d1c939fba878
root <root@DESKTOP-HVH29MM.localdomain>
2024-06-20 02:05:59 -0300

    Agrego de index.html y ruta en app.py

--- backend/app.py
+++ backend/app.py
@@ -1,15 +1,20 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_cors import CORS
 from models import db
 from config import Config
 from productRouter import productos
 from carritoRouter import carritos
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='../frontend/static', template_folder='../frontend/templates')
 app.config.from_object(Config)
 port = 5000
 CORS(app)
 db.init_app(app)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
 
 app.register_blueprint(productos)
 app.register_blueprint(carritos)
