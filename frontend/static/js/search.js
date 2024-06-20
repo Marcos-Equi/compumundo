5c5e4e3eb9ebca4a450019f7dd028c7f268c88d5
root <root@DESKTOP-HVH29MM.localdomain>
2024-06-20 05:36:53 -0300

    arreglando logica de search.js

--- frontend/static/js/search.js
+++ frontend/static/js/search.js
@@ -1,1 +1,4 @@
-"hola"
+document.getElementById('search-button').addEventListener('click', function() {
+    var query = document.getElementById('search-input').value;
+    window.location.href = `/producto/${query}`;
+});
