c55d6f8d78b56d48b99e9c38b20db9269f9a39b2
ezequiel-acosta <jeacosta@fi.uba.ar>
2024-06-21 17:16:49 -0300

    Agrego endpoints de creacion y mostrar contenido del carrito

--- backend/carritoRouter.py
+++ backend/carritoRouter.py
@@ -1,8 +1,34 @@
 from flask import Blueprint, request, jsonify
+from models import Carrito, ProdCarrito, Producto, db
 
 carritos = Blueprint('carritos', __name__, url_prefix='/carritos')
 
+@carritos.route('/<int:id>', methods=['GET'])
+def get_cart(id):
+    carrito = Carrito.buscar_por_id(id)
+    if not carrito:
+        return jsonify({'error': 'Carrito no existente'}), 404
+    items_carrito = ProdCarrito.buscar_por_carrito(id)
+    lista_items = []
+    for item in items_carrito:
+        producto = Producto.buscar_por_id(item.producto_id)
+        info_producto = {
+            'nombre': producto.nombre,
+            'tipo': producto.tipo,
+            'precio': producto.precio,
+            'descripcion': producto.descripcion
+        }
+        lista_items.append(item.serialize(info_producto))
+    return jsonify({'carrito': carrito.serialize(lista_items)}), 200
 
-@carritos.route('/', methods=['GET'])
-def get_products():
-    return jsonify({'carritos': 'Este es mi carrito de compras'})
+@carritos.route('/', methods=['POST'])
+def create_cart():
+    data = request.get_json()
+    carrito = Carrito(nombre_usuario=data['nombre'])
+    try:
+        db.session.add(carrito)
+        db.session.commit()
+    except Exception as e:
+        db.session.rollback()
+        return jsonify({'error': str(e)}), 500
+    return jsonify({'carrito': carrito.serialize([])}), 201
