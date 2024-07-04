3e7c279c43ae5939edd59b188282c688f55ee45b
Marcos-Equi <marcaceres@fi.uba.ar>
2024-07-04 19:02:10 -0300

    Agrego autoplay de swaper en destacados.

--- frontend/static/js/featured.js
+++ frontend/static/js/featured.js
@@ -1,25 +1,7 @@
-const swiper = new Swiper('.swiper', {
-    // Optional parameters
-    slidesPerView: 4,
-    spaceBetween: 10,
-    loop: true,
-    centerSlide: 'true',
-    pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-        dynamicBullets: true,
-    },
-    // Navigation arrows
-    navigation: {
-        nextEl: ".fa-arrow-right",
-        prevEl: ".fa-arrow-left",
-    }
-});
-
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener('DOMContentLoaded', async function () {
     var featuredProductsContainer = document.getElementById('featured-products-container');
 
-    fetch('/api/productos/destacados')
+    await fetch('/api/productos/destacados')
         .then(response => response.json())
         .then(data => {
             if (data.error) {
@@ -30,7 +12,7 @@ document.addEventListener('DOMContentLoaded', function () {
             data.productos.forEach(producto => {
                 var productDiv = document.createElement('div');
                 productDiv.classList.add('featured-product', 'swiper-slide');
-                productDiv.setAttribute('data-swiper-autoplay', '5000')
+                // productDiv.setAttribute('data-swiper-autoplay', '5000')
                 productDiv.addEventListener('click', function () {
                     window.location.href = `/producto?id=${producto.id}`;
                 });
@@ -61,9 +43,28 @@ document.addEventListener('DOMContentLoaded', function () {
 
                 featuredProductsContainer.appendChild(productDiv);
             });
-            swiper.update();
         })
         .catch(error => {
             console.error('Error al obtener productos destacados:', error);
         });
+
+    const swiper = new Swiper('.swiper', {
+        slidesPerView: 4,
+        spaceBetween: 10,
+        loop: true,
+        centerSlide: 'true',
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+            dynamicBullets: true,
+        },
+        autoplay: {
+            delay: 4000
+        },
+        navigation: {
+            nextEl: ".fa-arrow-right",
+            prevEl: ".fa-arrow-left",
+        }
+    });
 })
+
