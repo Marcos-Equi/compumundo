f0dec4de632629621f8c9b5e5d8fe5d3688acf73
Marcos-Equi <marcaceres@fi.uba.ar>
2024-07-03 22:07:27 -0300

    Agrego swiper

--- frontend/static/js/featured.js
+++ frontend/static/js/featured.js
@@ -1,3 +1,21 @@
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    slidesPerView: 4,
+    spaceBetween: 10,
+    loop: true,
+    centerSlide: 'true',
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+        dynamicBullets: true,
+    },
+    // Navigation arrows
+    navigation: {
+        nextEl: ".fa-arrow-right",
+        prevEl: ".fa-arrow-left",
+    }
+});
+
 document.addEventListener('DOMContentLoaded', function () {
     var featuredProductsContainer = document.getElementById('featured-products-container');
 
@@ -11,7 +29,8 @@ document.addEventListener('DOMContentLoaded', function () {
 
             data.productos.forEach(producto => {
                 var productDiv = document.createElement('div');
-                productDiv.classList.add('featured-product');
+                productDiv.classList.add('featured-product', 'swiper-slide');
+                productDiv.setAttribute('data-swiper-autoplay', '5000')
                 productDiv.addEventListener('click', function () {
                     window.location.href = `/producto?id=${producto.id}`;
                 });
@@ -34,10 +53,17 @@ document.addEventListener('DOMContentLoaded', function () {
                 productPrice.classList.add('precio_prod');
                 productDiv.appendChild(productPrice);
 
+                let addToCartButton = document.createElement('a');
+                addToCartButton.href = '#';
+                addToCartButton.textContent = 'Agregar al carrito';
+                addToCartButton.classList.add('btn', 'btn-primary', 'mt-auto', 'agregar_car');
+                productDiv.appendChild(addToCartButton);
+
                 featuredProductsContainer.appendChild(productDiv);
             });
+            swiper.update();
         })
         .catch(error => {
             console.error('Error al obtener productos destacados:', error);
         });
-});
+})
