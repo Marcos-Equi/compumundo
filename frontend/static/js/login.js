5c977142551337d417f671fefdd75d732ba8134b
root <root@DESKTOP-HVH29MM.localdomain>
2024-07-05 10:57:19 -0300

    agregado funciona para recuperar contraseña con metodo PUT

--- frontend/static/js/login.js
+++ frontend/static/js/login.js
@@ -1,8 +1,9 @@
 const toggleFormButton = document.getElementById('toggleFormButton');
 const loginForm = document.getElementById('loginForm');
 const registerForm = document.getElementById('registerForm');
+const passwordRecoveryForm = document.getElementById('passwordRecoveryForm');
 const noAccountText = document.getElementById('noAccountText');
-const loginError = document.getElementById('loginError');
+const loginErrorButton = document.getElementById('loginErrorButton');
 
 toggleFormButton.addEventListener('click', () => {
     if (loginForm.style.display === 'none') {
@@ -18,10 +19,18 @@ toggleFormButton.addEventListener('click', () => {
     }
 });
 
+loginErrorButton.addEventListener('click', () => {
+    loginForm.style.display = 'none';
+    registerForm.style.display = 'none';
+    passwordRecoveryForm.style.display = 'block';
+    loginErrorButton.style.display = 'none';
+});
+
 function registerUser() {
     const nombre = document.getElementById('nombre').value;
     const apellido = document.getElementById('apellido').value;
     const contraseña = document.getElementById('contraseña').value;
+    const respuesta = document.getElementById('respuestaSeguridad').value;
 
     fetch('/usuarios/register', {
         method: 'POST',
@@ -31,19 +40,24 @@ function registerUser() {
         body: JSON.stringify({
             nombre: nombre,
             apellido: apellido,
-            contraseña: contraseña
+            contraseña: contraseña,
+            respuesta: respuesta
         })
     })
     .then(response => response.json())
     .then(data => {
         if (data.message) {
             window.location.href = '/';
+        } else {
+            alert('Error al registrar usuario: ' + data.error);
         }
     })
     .catch(error => console.error('Error:', error));
 }
 
 function loginUser() {
+    document.getElementById('loginErrorButton').style.display = 'none';
+
     const nombre = document.getElementById('username').value;
     const contraseña = document.getElementById('password').value;
 
@@ -60,10 +74,38 @@ function loginUser() {
     .then(response => response.json())
     .then(data => {
         if (data.message === 'Inicio de sesión exitoso') {
-            localStorage.setItem('usuario', nombre); 
+            localStorage.setItem('usuario', nombre);
             window.location.href = '/';
         } else {
-            document.getElementById('loginError').style.display = 'block';
+            document.getElementById('loginErrorButton').style.display = 'block';
+        }
+    })
+    .catch(error => console.error('Error:', error));
+}
+
+function recoverPassword() {
+    const usernameRecovery = document.getElementById('usernameRecovery').value;
+    const preguntaSeguridadRecovery = document.getElementById('preguntaSeguridadRecovery').value;
+    const respuestaSeguridadRecovery = document.getElementById('respuestaSeguridadRecovery').value;
+    const newPassword = document.getElementById('newPassword').value;
+
+    fetch(`/usuarios/${usernameRecovery}/recover-password`, {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            preguntaSeguridad: preguntaSeguridadRecovery,
+            respuestaSeguridad: respuestaSeguridadRecovery,
+            nuevaContraseña: newPassword
+        })
+    })
+    .then(response => response.json())
+    .then(data => {
+        if (data.message === 'Contraseña actualizada correctamente') {
+            window.location.href = '/';
+        } else {
+            alert('Error al recuperar la contraseña: ' + data.error);
         }
     })
     .catch(error => console.error('Error:', error));
