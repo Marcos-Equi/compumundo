6fa3045c5066f9de37a0e8b294ecaee7c7dac094
Marcos-Equi <marcaceres@fi.uba.ar>
2024-07-08 16:20:17 -0300

    Agrego mis compras

--- frontend/static/js/admin.js
+++ frontend/static/js/admin.js
@@ -1,17 +1,18 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const miCuentaBtn = document.getElementById('mi-cuenta');
-    const misComprasBtn = document.getElementById('mis-compras');
+const misComprasBtn = document.getElementById('mis-compras');
+const misComprasContent = document.getElementById('mis-compras-content');
+const miCuentaBtn = document.getElementById('mi-cuenta');
+const miCuentaContent = document.getElementById('mi-cuenta-content');
+
+document.addEventListener('DOMContentLoaded', function () {
     const salirBtn = document.getElementById('salir');
-    
-    const miCuentaContent = document.getElementById('mi-cuenta-content');
-    const misComprasContent = document.getElementById('mis-compras-content');
+
     const changePasswordForm = document.getElementById('change-password-form');
     const passwordChangeMessage = document.getElementById('password-change-message');
 
     function llenarDatosUsuario() {
-        var nombreGuardado = localStorage.getItem('usuario');
-        var inputNombre = document.getElementById('nombre');
-        var inputApellido = document.getElementById('apellido');
+        let nombreGuardado = localStorage.getItem('usuario');
+        let inputNombre = document.getElementById('nombre');
+        let inputApellido = document.getElementById('apellido');
 
         if (nombreGuardado) {
             inputNombre.value = nombreGuardado;
@@ -36,24 +37,24 @@ document.addEventListener('DOMContentLoaded', function() {
 
     llenarDatosUsuario();
 
-    miCuentaBtn.addEventListener('click', function() {
+    miCuentaBtn.addEventListener('click', function () {
         miCuentaContent.style.display = 'block';
         misComprasContent.style.display = 'none';
+        let pedidos_container = document.querySelector('.pedidos_container');
+        if (pedidos_container) {
+            pedidos_container.remove();
+        }
         llenarDatosUsuario();
     });
 
-    misComprasBtn.addEventListener('click', function() {
-        misComprasContent.style.display = 'block';
-        miCuentaContent.style.display = 'none';
-    });
 
-    salirBtn.addEventListener('click', function() {
+    salirBtn.addEventListener('click', function () {
         window.location.href = '/';
     });
 
-    changePasswordForm.addEventListener('submit', function(event) {
+    changePasswordForm.addEventListener('submit', function (event) {
         event.preventDefault();
-        
+
         const nombreGuardado = localStorage.getItem('usuario');
         const oldPassword = document.getElementById('old-password').value;
         const newPassword = document.getElementById('new-password').value;
@@ -75,20 +76,84 @@ document.addEventListener('DOMContentLoaded', function() {
                 newPassword: newPassword
             })
         })
+            .then(response => response.json())
+            .then(data => {
+                if (data.error) {
+                    passwordChangeMessage.textContent = data.error;
+                    passwordChangeMessage.className = "error";
+                } else {
+                    passwordChangeMessage.textContent = data.message;
+                    passwordChangeMessage.className = "success";
+                }
+            })
+            .catch(error => {
+                passwordChangeMessage.textContent = 'Error al cambiar la contraseña';
+                passwordChangeMessage.className = "error";
+                console.error('Error al cambiar la contraseña:', error);
+            });
+    });
+});
+
+
+misComprasBtn.addEventListener('click', async function () {
+    misComprasContent.style.display = 'block';
+    miCuentaContent.style.display = 'none';
+
+    let idUsuario = localStorage.getItem('usuario_id');
+    await fetch(`/api/carritos/usuario/${idUsuario}`)
         .then(response => response.json())
         .then(data => {
             if (data.error) {
-                passwordChangeMessage.textContent = data.error;
-                passwordChangeMessage.className = "error";
-            } else {
-                passwordChangeMessage.textContent = data.message;
-                passwordChangeMessage.className = "success";
+                console.error('Error al obtener carritos:', data.error);
+                return;
             }
+            let carritos = data.compras;
+            console.log(data);
+
+            let pedidos = document.createElement('div');
+            pedidos.classList.add('col-md-9', 'mx-auto', 'pedidos_container')
+            let tituloPedidos = document.createElement('h3');
+            tituloPedidos.textContent = 'Mis compras';
+            tituloPedidos.classList.add('titulo_container');
+            pedidos.appendChild(tituloPedidos);
+
+            carritos.forEach(carrito => {
+                let fecha = new Date(carrito.fecha_creacion);
+                let fechaFormateada = `${fecha.getDate()}/${fecha.getMonth() + 1}/${fecha.getFullYear()}`;
+
+
+                pedidos.innerHTML += `
+                <div class="row gx-2 card"  data-product-id="${carrito.id}">
+                    <div class="col-12 mb-3">
+                        <div class="d-flex justify-content-between">
+                            <div>
+                                <span class="title_card">N° de pedido: </span>
+                                <span class="numPedido">${carrito.id}</span>
+                            </div>
+                            <div>
+                                <span class="title_card">Fecha: </span>
+                                <span class="fecha">${fechaFormateada}</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-4 mb-3">
+                        <div class="d-flex flex-row">
+                            <span class="title_card me-1">Cantidad de productos:</span><span class="cantProductos">  ${carrito.items.length}</span>
+                        </div>
+                    </div>
+                    <div class="col-12">
+                        <div class="d-flex justify-content-between">
+                            <div>
+                            <span class="title_card">Monto: </span>
+                            <span>$${parseFloat(carrito.precio_total).toLocaleString("es-ES")}</span>
+                            </div>
+                            <div>
+                                <button class="modalDetalles">Ver detalle</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>`
+                misComprasContent.appendChild(pedidos);
+            })
         })
-        .catch(error => {
-            passwordChangeMessage.textContent = 'Error al cambiar la contraseña';
-            passwordChangeMessage.className = "error";
-            console.error('Error al cambiar la contraseña:', error);
-        });
-    });
 });
